ecd10d8b77620113a09555aac517614344450531
roman-marken <markenkoroman@gmail.com>
2025-02-08 05:21:17 +0200

    Add files via upload

---
+++ peremen/script.js
@@ -0,0 +1,13 @@
+//вывод текста в кансоль
+console.info("JavaScript Hllo");
+//переменая
+var num = 5;
+//num = 7;
+console.info("Переменая: " + num)
+//const - переменая в которой нельзя изминить значение
+//var - переменя в каторой в любой момент можно изминить значение
+const name = "Hi";
+
+var number;
+number = false;
+
